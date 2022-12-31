05937f2da2ec73d07931cc88282d91546d5c1548
TK <thiratikan@gmail.com>
2022-12-30 19:32:21 -0500

    update pictures files

--- src/Component/Experience.js
+++ src/Component/Experience.js
@@ -4,15 +4,28 @@ import { Title } from "../style/PortStyle";
 
 function Experience() {
   const Container = styled.section`
+    blackground-color: white;
     margin-top: 300px;
+    p {
+      font-family: "AvenirHeavy";
+      color: #eee;
+      font-size: 200px;
+      margin-left: 50px;
+      margin-top: -155px;
+    }
+    div {
+    }
   `;
 
   const Body = styled.div``;
   return (
     <>
       <Container>
-        <Title id="experience">EXPERIENCE</Title>
-        <Body></Body>
+        <div>
+          <Title id="experience">EXPERIENCE</Title>
+          <p>& SKILLS</p>
+          <Body></Body>
+        </div>
       </Container>
     </>
   );
