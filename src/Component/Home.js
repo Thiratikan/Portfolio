f29103cb9cfe2f85c7da7f03a018cbceb099c1f2
Thiratikan <thiratikan@gmail.com>
2023-01-01 20:26:32 -0500

    fixed bug

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,15 +3,12 @@ import Introduce from "./Introduce";
 import Experience from "./Experience";
 import Projects from "./Projects";
 import Contact from "./Contact";
-import { Title } from "../style/PortStyle";
 
 function Home() {
   return (
     <>
       <Navbar />
-
       <Introduce />
-
       <Projects />
       <Experience />
       <Contact />
