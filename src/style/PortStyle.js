e6fd8104f281f4dd60961ce0b44373e81463d264
Thiratikan <91763397+Thiratikan@users.noreply.github.com>
2023-01-25 16:26:33 -0500

    Update PortStyle.js

--- src/style/PortStyle.js
+++ src/style/PortStyle.js
@@ -1,98 +1,78 @@
 import styled from "styled-components";
 
 export const PROJECT = styled.div`
-margin-top: 200px;
-position: absolute;
-left: 13%;
-
+  margin-top: 150px;
+  .DIV {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 150px;
+    margin: auto;
+    align-items: center;
+    margin-bottom: 150px;
+  }
+  .detail {
+    margin-left: 100px;
+  }
   img {
     height: 450px;
+  }
 
-    margin-bottom: 100px;
-  }
-  div {
-    background-image: none;
-    display: grid;
-  }
   h1 {
     font-family: "Gugi", cursive;
     font-size: 80px;
-    position: absolute;
-    margin-left: 950px;
-    margin-top: -15px;
-    width: 200px;
   }
   h2 {
     font-family: "Roboto", sans-serif;
     text-indent: 80px;
-    position: absolute;
-    width: 650px;
-    margin-left: 720px;
-    margin-top: 110px;
+    width: 700px;
   }
   .title {
     font-family: "AvenirHeavy";
-  color: #e5e5e5;
-  font-size: 160px;
-  position: absolute;
-  z-index: -1;
-  margin-left: 620px;
-  margin-top: 0px;
- }
- a img{
-  display: flex;
-  height: 25px;
-}
-
-.link {
-  margin-left: 720px;
-  margin-top: -180px;
-}
-
-.github {
-  margin-left: 770px;
-  margin-top: -180px;
-}
+    color: #e5e5e5;
+    font-size: 160px;
+    position: absolute;
+    z-index: -1;
+    margin-left: 550px;
+  }
+  a img {
+    height: 25px;
+  }
+  .github {
+    margin-left: 20px;
+  }
 
   @media only screen and (max-width: 600px) {
-    margin-top: 0px;
-    left: 1%;
+    margin-top: 100px;
     img {
       height: 250px;
-      width: 300px;
-      margin: 0;
-      align-items: center;
-      padding: 20px;
-      margin-left: 20px;
-      margin-bottom: 180px;
+      width: 250px;
     }
-    div {
-      background-image: none;
-      display: grid;
+    a img {
+      height: 25px;
+      width: 25px;
+    }
+    .detail {
+      margin-left: 20px;
+    }
+    .DIV {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 50px;
     }
     h1 {
       font-family: "Gugi", cursive;
-      font-size: 30px;
-      position: absolute;
-      margin-left: 20px;
-      margin-top: 280px;
-     
-      
+      font-size: 40px;
     }
     h2 {
-      text-indent: 20px;
-      position: absolute;
-      font-size: 15px; 
+      font-family: "Roboto", sans-serif;
+      text-indent: 80px;
       width: 350px;
-      padding: 20px;
+      font-size: 20px;
+    }
+    .title {
+      font-size: 70px;
       margin-left: 0px;
-      margin-top: 305px;
-
+      margin-top: 200px;
+    }
   }
-  .title {
-    font-size: 78px;
-    margin-left: 0px;
-    margin-top: 290px;
-  }
- 
 `;
